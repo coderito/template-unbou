3c62e137cf5bee3b6e77aec4fcb628d457e435f0
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-08-17 00:29:04 -0400

    delete logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/images/logo";
 import { Button } from "../Button/Button";
 
 export const Navbar = () => {
   return (
     <nav className="md:px-40 px-10 flex justify-between items-center py-7 shadow-md z-10">
       <Link to={"/"}>
-        <Logo />
+        Unbou
       </Link>
 
       <div className="hidden md:flex">
